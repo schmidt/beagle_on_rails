e77c66e67e7b4bd295ed43253a6f8de7c005f71b
Gregor Schmidt <ruby@schmidtwisser.de>
2006-04-21 12:26:29 +0000

    Ruby-WS-test-Client eingebaut

---
+++ test/others/beagle_api_test.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+require 'soap/wsdlDriver'
+WSDL_URI = 'http://localhost:3000/beagle/service.wsdl'
+
+class BeagleControllerApiTest < Test::Unit::TestCase
+	def setup
+	        factory = SOAP::WSDLDriverFactory.new WSDL_URI
+        	@beagle = factory.create_rpc_driver
+	end
+
+	def test_execute
+		search_string = "Hallo"
+		search_model = @beagle.execute search_string
+
+		assert_equal search_model.key, search_string
+		assert_equal search_model.order, "score"
+
+		assert search_model.total_time > 0.0
+		assert search_model.number_of_hits > 0
+
+	end
+end
